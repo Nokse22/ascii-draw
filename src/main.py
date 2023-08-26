239cfaeb5690da02005253992b603f106aaf5a3b
Nokse22 <lorenzo.ubaldi22@gmail.com>
2023-08-26 16:35:53 +0200

    updated to 0.1.6

--- src/main.py
+++ src/main.py
@@ -134,7 +134,7 @@ class AsciiDrawApplication(Adw.Application):
                                 application_name='ASCII Draw',
                                 application_icon='io.github.nokse22.asciidraw',
                                 developer_name='Nokse',
-                                version='0.1.5',
+                                version='0.1.6',
                                 website='https://github.com/Nokse22/ascii-draw',
                                 issue_url='https://github.com/Nokse22/ascii-draw/issues',
                                 developers=['Nokse'],
