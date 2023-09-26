3404af4a240c9cab2007f3c1fd35072e626568ae
Nokse22 <lorenzo.ubaldi22@gmail.com>
2023-09-26 22:38:52 +0200

    update appdata and version

--- src/main.py
+++ src/main.py
@@ -162,7 +162,7 @@ class AsciiDrawApplication(Adw.Application):
                                 application_name=_("ASCII Draw"),
                                 application_icon='io.github.nokse22.asciidraw',
                                 developer_name='Nokse',
-                                version='0.1.9',
+                                version='0.2.0',
                                 website='https://github.com/Nokse22/ascii-draw',
                                 issue_url='https://github.com/Nokse22/ascii-draw/issues',
                                 developers=['Nokse'],
